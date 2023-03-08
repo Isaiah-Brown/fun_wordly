fa9b24f04dce10f2a991e69c6e89e6ae63663e4e
tracyyang816 <yyang5@fandm.edu>
2023-03-08 15:37:15 -0500

    launcher/explaination page has an onclick now

--- app/src/main/java/awesome/zaza/cat/wordly_explained.java
+++ app/src/main/java/awesome/zaza/cat/wordly_explained.java
@@ -2,7 +2,10 @@ package awesome.zaza.cat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 
 public class wordly_explained extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class wordly_explained extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wordly_explained);
+
+        View v = findViewById(R.id.start_page);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Main.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
